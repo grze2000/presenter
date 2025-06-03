eedf61b0c4df69c491617ef1f3259ef9f43cb375
Grzegorz Babiarz <grzegorz.babiarz@cetuspro.com>
2025-06-03 18:45:49 +0200

    fixes for node 16

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./src/renderer",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   publicDir: false,
   base: "./",
   build: {
